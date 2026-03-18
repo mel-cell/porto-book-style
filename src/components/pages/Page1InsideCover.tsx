3355c29997a467a06becac13789b190370ada8cf
mel-cell <melmel67799@gmail.com>
2026-03-18 17:50:15 +0700

    fix: update ui page 1

--- src/components/pages/Page1InsideCover.tsx
+++ src/components/pages/Page1InsideCover.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import Image from "next/image";
 import { motion } from "framer-motion";
 import styles from "./page.module.css";
+import s from "./Page1InsideCover.module.css";
 
 const containerVariants = {
   hidden: { opacity: 0 },
   visible: {
     opacity: 1,
     transition: {
-      duration: 0.5
+      duration: 0.8
     },
   }
 };
@@ -16,14 +18,25 @@ const containerVariants = {
 export default function Page1InsideCover() {
   return (
     <motion.div 
-      className={styles.page} 
+      className={`${styles.page} ${s.pageBlack}`} 
       data-page="1"
       variants={containerVariants}
       initial="hidden"
       animate="visible"
     >
-        <div className={styles.pageNumber}>01</div>
-        {/* Page 1 is now empty */}
+      <div className={s.contentCenter}>
+        <div className={s.imageContainer}>
+          <Image 
+            src="/img/bla3.png" 
+            alt="bla bla bla" 
+            fill 
+            className={s.invertImage}
+            sizes="50vw"
+            priority
+          />
+        </div>
+      </div>
+      <div className={`${styles.pageNumber} ${s.pageNumberWhite}`}>01</div>
     </motion.div>
   );
 }
